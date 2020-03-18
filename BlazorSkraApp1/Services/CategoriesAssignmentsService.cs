d512cbd1aa2ab8c29cbcb372d57c7120e5a28a4c
Benedikt Runar <benedikt17@ru.is>
2020-03-18 11:32:51 +0000

    Fetching list of Forms from CategoriesAssignments. Combining that table with the tables Categories and FormsInfo

---
+++ BlazorSkraApp1/Services/CategoriesAssignmentsService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorSkraApp1.Data;
+
+namespace BlazorSkraApp1.Services
+{
+    public interface ICategoriesAssignmentsService
+    {
+        Task<List<CategoriesAssignments>> Get();
+        Task<CategoriesAssignments> Get(int id);
+        //Task<CategoriesAssignments> Add(CategoriesAssignments CategoriesAssignment);
+        //Task<CategoriesAssignments> Update(CategoriesAssignments CategoriesAssignment);
+        //Task<CategoriesAssignments> Delete(int id);
+    }
+
+    public class CategoriesAssignmentsService : ICategoriesAssignmentsService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesAssignmentsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CategoriesAssignments>> Get()
+        {
+            return await _context.CategoriesAssignments
+                .Include(c => c.Categories)
+                .Include(f => f.FormsInfo)
+                .ToListAsync();
+        }
+
+        public async Task<CategoriesAssignments> Get(int CategoryId)
+        {
+            var categoriesAssignments = await _context.CategoriesAssignments.FindAsync(CategoryId);
+            return categoriesAssignments;
+        }
+    }
+}
