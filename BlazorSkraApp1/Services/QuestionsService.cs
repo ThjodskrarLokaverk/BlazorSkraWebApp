b76b5b46d6c6813ba7d698953e16ceb4bae4b851
Benedikt Runar <benedikt17@ru.is>
2020-03-27 09:52:15 +0000

    Fetching data for notification view for users

---
+++ BlazorSkraApp1/Services/QuestionsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorSkraApp1.Data;
+
+namespace BlazorSkraApp1.Services
+{
+    public class QuestionsService
+    {
+    }
+}
