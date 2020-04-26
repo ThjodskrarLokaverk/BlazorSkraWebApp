129d8a65112fc085d0d56d2cc45890188e592d16
thornystefans <thorny17@ru.is>
2020-04-26 11:07:02 +0000

    .

--- BlazorSkraApp1/Models/InputModels/SubmissionInputModel.cs
+++ BlazorSkraApp1/Models/InputModels/SubmissionInputModel.cs
@@ -8,17 +8,45 @@ namespace BlazorSkraApp1.Models.InputModels
     {
         public int FormId { get; set; }
         [ValidateComplexType]
-        public RequiredString[] Answers { get; set; }
-        public int[] AnswersOrderNum { get; set; }
+        public RequiredShortText[] ShortText { get; set; }
+        [ValidateComplexType]
+        public RequiredLongText[] LongText { get; set; }
+        [ValidateComplexType]
+        public RequiredRadio[] Radio { get; set; }
+        [ValidateComplexType]
+        public RequiredCheckbox[] Checkbox { get; set; }
+        [ValidateComplexType]
+        public RequiredDate[] Date { get; set; }
+        //public int[] AnswersOrderNum { get; set; }
         public List<string> MultipleAnswers { get; set; }
-        public bool Confirmation { get; set; }
         public DateTime SelectedDate { get; set; }
     }
 
-    public class RequiredString
+    public class RequiredShortText
     {
         [Required(ErrorMessage = "Reitur má ekki vera auður")]
-        [StringLength(500, ErrorMessage = "Hámarks stafafjöldi er 500 stafir")]
+        [StringLength(100, ErrorMessage = "Hámarks stafafjöldi er 100 stafir")]
+        public string Value { get; set; }
+    }
+    public class RequiredLongText
+    {
+        [Required(ErrorMessage = "Reitur má ekki vera auður")]
+        [StringLength(4000, ErrorMessage = "Hámarks stafafjöldi er 4000 stafir")]
+        public string Value { get; set; }
+    }
+    public class RequiredRadio
+    {
+        [Required(ErrorMessage = "Velja verður einn valkost")]
+        public string Value { get; set; }
+    }
+    public class RequiredCheckbox
+    {
+        [Required(ErrorMessage = "Velja verður að minnsta kosti einn valkost")]
+        public string Value { get; set; }
+    }
+    public class RequiredDate
+    {
+        [Required(ErrorMessage = "ATH date")]
         public string Value { get; set; }
     }
 }
