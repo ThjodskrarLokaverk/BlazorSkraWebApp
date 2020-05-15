f11e470f36e7693bdfcc30a760e0b069557ec233
Benedikt Runar <benedikt17@ru.is>
2020-05-15 10:51:53 +0000

    Reviewing system

--- BlazorSkraApp1/Models/InputModels/QuestionsInputModel.cs
+++ BlazorSkraApp1/Models/InputModels/QuestionsInputModel.cs
@@ -9,7 +9,7 @@ namespace BlazorSkraApp1.Models.InputModels
     public class QuestionsInputModel
     {
         [Required(ErrorMessage = "Velja þarf titil spurningar")]
-        [StringLength(50, ErrorMessage = "Spurning má ekki innihalda fleiri en 50 stafi")]
+        [StringLength(100, ErrorMessage = "Spurning má ekki innihalda fleiri en 100 stafi")]
         public string QuestionName { get; set; }
         [Required(ErrorMessage = "Velja þarf spurningategund")]
         public string QuestionType { get; set; }
